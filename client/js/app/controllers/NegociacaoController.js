a93575b7c0ccce9ea99d4fe3e672b1820f6b4b0b
Gabriel Victor Raymundo <bieelvii@gmail.com>
2022-04-16 13:02:14 -0300

    Criado o controller negociacao e sua logica. Criado a model negociacao e seus atributos. Criado a o helper data e sua logica

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,25 @@
+class NegociacaoController {
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let dataHelper = new DataHelper;
+
+        let negociacao = new Negociacao(
+            dataHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+        console.log(dataHelper.dataParaTexto(negociacao.data));
+    }
+}
